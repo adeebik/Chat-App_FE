921623e8ffe2b8b8c1351eda2b4de2fc1ca704ac
Khan-Adeeb <adeebkhan2.786@gmail.com>
2026-01-02 22:47:53 +0530

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [socket, setSocket] = useState();
-  const inputref = useRef<HTMLInputElement>();
+  const inputref = useRef<HTMLInputElement>(null);
 
   function sendMessage() {
-    socket?.send(inputref.current.value);
+    if (!socket) {
+      return null;
+    }
+    socket.send(inputref.current?.value);
   }
 
   useEffect(() => {
@@ -15,14 +18,19 @@ function App() {
     ws.onmessage = (e) => {
       alert(e.data);
     };
+    
   }, []);
 
   return (
-    <div className="h-screen w-screen  flex items-center justify-center">
-      <div className="max-h-250 h-full max-w-150 w-full bg-zinc-900 border border-zinc-400 rounded-xl overflow-hidden flex flex-col justify-between  ">
-        <div className="msgBox h-full flex flex-col  w-full rounded-b p-3" > 
-          <div className="mymsg self-end my-1 bg-blue-600 text-white px-2 py-1 rounded-xl">Hi there</div>
-          <div className="yourmsg my-1 bg-green-600 text-white px-2 py-1 rounded-xl w-fit">Hello  wasuup</div>
+    <div className="h-screen w-screen bg-black flex items-center justify-center p-5">
+      <div className="max-h-160 h-full max-w-130 w-full  border border-zinc-400 rounded-xl overflow-hidden flex flex-col justify-between  ">
+        <div className="msgBox h-full flex flex-col  w-full rounded-b p-3">
+          <div className="mymsg self-end my-1 bg-blue-600 text-white px-3 py-1 rounded-xl">
+            Hi there
+          </div>
+          <div className="yourmsg my-1 bg-green-600 text-white px-3 py-1 rounded-xl w-fit">
+            Hello wasuup?
+          </div>
         </div>
         <div className="textBox p-5 gap-5 w-full flex">
           <input
